914bfc8930a30d935aebd84241ee66b6499bedae
Leonardo S. Viana <94654528+leosviana@users.noreply.github.com>
2025-03-17 18:01:58 -0300

    atualizando arquivos

--- calculos.js
+++ calculos.js
@@ -5,12 +5,17 @@ var btCalcular = document.getElementById("botao-calcular");
 function botaoCalcular(){
     var valorInicial = document.getElementById("valor-inicial").value;
     var valorMensal = document.getElementById("valor-mensal").value;
-    var TaxaRentabilidade = document.getElementById("taxa-rentabilidade").value;
-    var TempoRentabilidade = document.getElementById("tempo-rentabilidade").value;
-    var TaxaInflacao = document.getElementById("taxa-inflacao").value;
-    var TempoInflacao = document.getElementById("tempo-inflacao").value;
-    var TaxaInvestimento = document.getElementById("taxa-investimento").value;
-    var TempoInvestimento = document.getElementById("tempo-investimento").value;
-    console.log(valorInicial + "-" + valorMensal + "-" + TaxaRentabilidade + "-" + TempoRentabilidade + "-" + TaxaInflacao + "-" + TempoInflacao "-" + TaxaInvestimento + "-" + TempoInvestimento);
+    var taxaRentabilidade = document.getElementById("taxa-rentabilidade").value;
+    var tempoRentabilidade = document.getElementById("tempo-rentabilidade").value;
+    var taxaInflacao = document.getElementById("taxa-inflacao").value;
+    var tempoInflacao = document.getElementById("tempo-inflacao").value;
+    var periodoInvestimento = document.getElementById("periodo-investimento").value;
+    var tempoInvestimento = document.getElementById("tempo-investimento").value;
+    //console.log(valorInicial + "-" + valorMensal + "-" + TaxaRentabilidade + "-" + TempoRentabilidade + "-" + TaxaInflacao + "-" + TempoInflacao + "-" + TaxaInvestimento + "-" + TempoInvestimento);
+
+    //CÁLCULO VALOR FUTURO:  
+    //valorFuturo = valorMensal * (((1 + taxaRentabilidade) ** tempoRentabilidade - 1) / taxaRentabilidade) * (1 + taxaRentabilidade);
+    valorFuturo = 500 * (((1 + 0.12) ** 12 - 1) / 12) * (1 + 12);
+    console.log(valorFuturo.toFixed(2));
 }
 
