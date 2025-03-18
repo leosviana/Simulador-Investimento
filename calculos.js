e970e382661d08273f93c04e168b81a893673e4f
Leonardo S. Viana <94654528+leosviana@users.noreply.github.com>
2025-03-18 11:06:17 -0300

    alterado arquivo

--- calculos.js
+++ calculos.js
@@ -13,9 +13,31 @@ function botaoCalcular(){
     var tempoInvestimento = document.getElementById("tempo-investimento").value;
     //console.log(valorInicial + "-" + valorMensal + "-" + TaxaRentabilidade + "-" + TempoRentabilidade + "-" + TaxaInflacao + "-" + TempoInflacao + "-" + TaxaInvestimento + "-" + TempoInvestimento);
 
+    //TAXA RENTABILIDADE (ANUAL/MENSAL):
+    var taxaRentabilidade = 12;
+    var taxaRentabilidadeMensal = (1 + (taxaRentabilidade / 100)) / 12;
+    console.log("taxaRentabilidadeMensal: " + taxaRentabilidadeMensal.toFixed(6));
+
+    //TAXA INFLAÇÃO (ANUAL/MENSAL):
+    var taxaInflacao = 5;
+    var taxaInflacaoMensal = (1 + (taxaInflacao / 100)) ** (1 / 12) - 1;
+    //valorFuturo = 500 * (((1 + 0.12) ** 12 - 1) / 12) * (1 + 12);
+    console.log("taxaInflacaoMensal: " + taxaInflacaoMensal.toFixed(6));
+
+    //TAXA RENTABILIDADE REAL:
+    var taxaRealMensal = (1 + taxaRentabilidadeMensal) / (1 + taxaInflacaoMensal) - 1;
+    console.log("taxaRealMensal: " + taxaRealMensal.toFixed(6));
+
+    //RENTABILIDADE REAL:
+    //VF = P * (((1 + taxaRealMensal) ** n - 1) / taxaRealMensal);
+    var ValorMensal = 500;
+    var numeroMeses = 12;
+    var valorFuturo = ValorMensal * (((1 + taxaRentabilidadeMensal) ** numeroMeses - 1) / taxaRentabilidadeMensal);
+    console.log("valorFuturo: " + valorFuturo.toFixed(2));
+
+
     //CÁLCULO VALOR FUTURO:  
     //valorFuturo = valorMensal * (((1 + taxaRentabilidade) ** tempoRentabilidade - 1) / taxaRentabilidade) * (1 + taxaRentabilidade);
-    valorFuturo = 500 * (((1 + 0.12) ** 12 - 1) / 12) * (1 + 12);
-    console.log(valorFuturo.toFixed(2));
+
 }
 
