4eb1af4967912baad0779d159e1644b8579f4f12
Leonardo S. Viana <94654528+leosviana@users.noreply.github.com>
2025-03-26 12:01:33 -0300

    atualizando arquivos

--- calculos.js
+++ calculos.js
@@ -11,6 +11,10 @@ function botaoCalcular(){
     var tempoInflacao = parseInt(document.getElementById("tempo-inflacao").value) || 0;
     var periodoInvestimento = parseFloat(document.getElementById("periodo-investimento").value) || 0;
     var tempoInvestimento = parseInt(document.getElementById("tempo-investimento").value) || 0;
+    var rendaValorAnual = parseFloat(document.getElementById("renda-valor-anual").value) || 0;
+    var rendaValorMensal = parseFloat(document.getElementById("renda-valor-mensal").value || 0);
+    var valorIR = parseFloat(document.getElementById("valor-ir").value || 0);
+    var valorFuturoComIR = parseFloat(document.getElementById("valor-futuro-com-ir").value || 0);
     
     //TAXA RENTABILIDADE (ANUAL/MENSAL):
     //var taxaRentabilidade = 12;
@@ -22,9 +26,17 @@ function botaoCalcular(){
     var taxaInflacaoMensal = (1 + (taxaInflacao / 100)) ** (1 / 12) - 1;
     console.log("taxaInflacaoMensal: " + taxaInflacaoMensal.toFixed(6));
 
-    //TAXA RENTABILIDADE REAL:
+    //TAXA RENTABILIDADE REAL(MENSAL) - RENTABILIDADE LIQUIDA DE INFLAÇÃO:
+    //A taxa de rentabilidade real de inflação representa o ganho real de um investimento após descontar a inflação. 
+    //Em outras palavras, mostra quanto seu investimento realmente cresceu em termos de poder de compra.
     var taxaRealMensal = (1 + taxaRentabilidadeMensal) / (1 + taxaInflacaoMensal) - 1;
-    console.log("taxaRealMensal: " + taxaRealMensal.toFixed(6));
+    console.log("taxaRealMensal: " + (taxaRealMensal * 100).toFixed(4));
+    document.getElementById("rentabilidade-mensal").value = (taxaRealMensal * 100).toFixed(4);
+
+    //TAXA RENTABILIDADE REAL(ANUAL) - RENTABILIDADE LIQUIDA DE INFLAÇÃO:
+    var taxaRealAnual = (1 + taxaRealMensal) ** 12 - 1;
+    console.log("taxaRealAnual: " + (taxaRealAnual * 100).toFixed(4));
+    document.getElementById("rentabilidade-anual").value = (taxaRealAnual * 100).toFixed(4);
 
     //RENTABILIDADE FUTURA TOTAL:
     //var valorInicial = 500;
@@ -42,30 +54,32 @@ function botaoCalcular(){
     //Valor total futuro:
     var resultValorFuturo = VF_inicial + VF_mensal;    
     console.log("resultValorFuturo: " + resultValorFuturo.toFixed(2));
-  //  document.getElementById("valor-futuro").value = resultValorFuturo.toFixed(2);
+    document.getElementById("valor-futuro").value = resultValorFuturo.toFixed(2);
 
     //VALOR INVESTIDO:
     var resultValorInvestido = valorInicial + (valorMensal * periodoInvestimento);
     console.log("resultValorInvestido: " + resultValorInvestido.toFixed(2));
-  //  document.getElementById("valor-presente").value = resultValorInvestido.toFixed(2);
+    document.getElementById("valor-presente").value = resultValorInvestido.toFixed(2);
 
     //VALOR EM JUROS (MENSAL / ANUAL):
     //Anual:
     var resultValorJurosAnual = resultValorFuturo - resultValorInvestido;
     console.log("resultValorJurosAnual: " + resultValorJurosAnual.toFixed(2));
-   // document.getElementById("rentabilidade-anual").value = resultValorJurosAnual.toFixed(2);
+    document.getElementById("renda-valor-anual").value = resultValorJurosAnual.toFixed(2);
 
     //Mensal:
     var resultValorJurosMensal = resultValorFuturo * taxaRentabilidadeMensal;
     console.log("resultValorJurosMensal: " + resultValorJurosMensal.toFixed(2));
-   // document.getElementById("rentabilidade-mensal").value = resultValorJurosMensal.toFixed(2);
+    document.getElementById("renda-valor-mensal").value = resultValorJurosMensal.toFixed(2);
 
     //IMPOSTO DE RENDA:
     var taxaIR = 0.20; //20%
     var resultValorIR = resultValorJurosAnual * taxaIR;
     console.log("IR: " + resultValorIR.toFixed(2));
+    document.getElementById("valor-ir").value = resultValorIR.toFixed(2);
 
     //VALOR TOTAL LÍQUIDO:
     var resultValorLiquido = resultValorFuturo - resultValorIR;
     console.log("resultValorLiquido: " + resultValorLiquido.toFixed(2));
+    document.getElementById("valor-futuro-com-ir").value = resultValorLiquido.toFixed(2);
 }
