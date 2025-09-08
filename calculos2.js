5bccb6620af32c3a7558946aaf11809d9e16d425
Leonardo S. Viana <94654528+leosviana@users.noreply.github.com>
2025-09-08 15:25:32 -0300

    alterando doc

--- calculos2.js
+++ calculos2.js
@@ -1,8 +1,11 @@
 //VARIAVEIS GLOBAIS DO PROJETO
 //Dados de entrada
-//var btCalcular = document.getElementById("botao-calcular");
 
-function verResultado(){
+window.onload = function(){
+    armazenaValoresEntrada(); //Traz os valores armazenados da página principal
+}
+
+function calcular(){
     var valorInicial = parseFloat(document.getElementById("valor-inicial").value = sessionStorage.getItem("valor-inicial")) || 0;
     var valorMensal = parseFloat(document.getElementById("valor-mensal").value) || 0;
     var taxaRentabilidade = parseFloat(document.getElementById("taxa-rentabilidade").value) || 0;
